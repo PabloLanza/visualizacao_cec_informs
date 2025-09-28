8ca89b3207be38d171632e2faa1bc935773876e9
Pablo Augusto Lanza França <pablolanza75@gmail.com>
2025-09-28 18:56:01 -0300

    Add files via upload

--- visu.py
+++ visu.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import func
 
+st.markdown("""
+                <style>
+                h1 {color: darkblue !important;
+                    text_align: center;
+                }
+                <style>""", unsafe_allow_html=True)
+
 st.title("PAINEL INTERATIVO CEC INFORMS")
 
 competicao_sel = st.multiselect("Selecione as Competições: ", ['Mineiro', 'Sul Americana', 'Brasileiro', 'Copa do Brasil', ''])
